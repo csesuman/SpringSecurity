aa9cb7fb30397502459531df74d9efb06e6cb828
Suman Bhadra <sumanbhadrasuman@gmail.com>
2023-06-25 22:38:21 +0400

    Complete Login Logout

---
+++ src/main/java/com/example/demo/auth/ApplicationUser.java
@@ -0,0 +1,64 @@
+package com.example.demo.auth;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class ApplicationUser implements UserDetails {
+
+    public final List<? extends  GrantedAuthority> grantedAuthorities;
+    public final String password;
+    public final String username;
+
+    public final boolean isAccountNonExpired;
+    public final boolean isAccountNonLocked;
+    public final boolean isCredentialsNonExpired;
+    public final boolean isEnabled;
+
+    public ApplicationUser(List<? extends GrantedAuthority> grantedAuthorities, String password, String username, boolean isAccountNonExpired, boolean isAccountNonLocked, boolean isCredentialsNonExpired, boolean isEnabled) {
+        this.grantedAuthorities = grantedAuthorities;
+        this.password = password;
+        this.username = username;
+        this.isAccountNonExpired = isAccountNonExpired;
+        this.isAccountNonLocked = isAccountNonLocked;
+        this.isCredentialsNonExpired = isCredentialsNonExpired;
+        this.isEnabled = isEnabled;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return grantedAuthorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return isAccountNonExpired;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return isAccountNonLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return isCredentialsNonExpired;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return isEnabled;
+    }
+}
